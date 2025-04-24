ab7c6da7075942c38d9c7d03ad9cd21ddefabebc
Shuang L <39909546-shuangjhu@users.noreply.replit.com>
2025-04-24 01:45:31 +0000

    update styling for selecttrigger

--- client/src/components/ui/select.tsx
+++ client/src/components/ui/select.tsx
@@ -17,7 +17,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input px-3 py-2 text-sm placeholder:text-muted-foreground ring-offset-background focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-10 w-full items-center justify-between rounded-md border border-input px-3 py-2 text-sm placeholder:text-muted-foreground ring-offset-background focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 focus:ring-0 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
